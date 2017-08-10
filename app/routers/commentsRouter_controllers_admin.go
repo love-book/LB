ae0d16b25a3871e86f0f88b2238f3044d9622e98
kasoly <773683464@qq.com>
2017-08-10 20:56:38 +0800

    提交主要的项目文件

---
+++ app/routers/commentsRouter_controllers_admin.go
@@ -0,0 +1,82 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+	beego.GlobalControllerRouter["app/controllers/admin:BooksController"] = append(beego.GlobalControllerRouter["app/controllers/admin:BooksController"],
+		beego.ControllerComments{
+			Method: "BookList",
+			Router: `/booklist`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["app/controllers/admin:BooksController"] = append(beego.GlobalControllerRouter["app/controllers/admin:BooksController"],
+		beego.ControllerComments{
+			Method: "Bookadd",
+			Router: `/bookadd`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["app/controllers/admin:BooksController"] = append(beego.GlobalControllerRouter["app/controllers/admin:BooksController"],
+		beego.ControllerComments{
+			Method: "Bookupdate",
+			Router: `/bookupdate`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["app/controllers/admin:BooksController"] = append(beego.GlobalControllerRouter["app/controllers/admin:BooksController"],
+		beego.ControllerComments{
+			Method: "Uploadfile",
+			Router: `/uploadfile`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["app/controllers/admin:BooksController"] = append(beego.GlobalControllerRouter["app/controllers/admin:BooksController"],
+		beego.ControllerComments{
+			Method: "Bookaddbycode",
+			Router: `/bookaddbycode`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["app/controllers/admin:UserController"] = append(beego.GlobalControllerRouter["app/controllers/admin:UserController"],
+		beego.ControllerComments{
+			Method: "Userlist",
+			Router: `/userlist`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["app/controllers/admin:UserController"] = append(beego.GlobalControllerRouter["app/controllers/admin:UserController"],
+		beego.ControllerComments{
+			Method: "Useradd",
+			Router: `/useradd`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["app/controllers/admin:UserController"] = append(beego.GlobalControllerRouter["app/controllers/admin:UserController"],
+		beego.ControllerComments{
+			Method: "Userdelete",
+			Router: `/userdelete`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["app/controllers/admin:UserController"] = append(beego.GlobalControllerRouter["app/controllers/admin:UserController"],
+		beego.ControllerComments{
+			Method: "Update",
+			Router: `/userupdate`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+}
