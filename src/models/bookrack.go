126905461754a0a2fe291c9d7c1a28b31506a39c
kasoly <773683464@qq.com>
2017-08-11 17:18:17 +0800

    update

--- src/models/bookrack.go
+++ src/models/bookrack.go
@@ -4,7 +4,7 @@ import "github.com/astaxie/beego/orm"
 
 type Bookrack struct {
 	Userid       string  `json:"userid" valid:"Required" orm:"pk;size(20);column(userid);"`
-	Books        []Books `json:"books" valid:"Required"`
+	Books        string `json:"books" valid:"Required"`
 	Updated_time  int64  `json:"updated_time"`
 }
 
