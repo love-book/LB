6b3e7470a2293227fed8deeed7fe7f2bb7a49aa8
kasoly <773683464@qq.com>
2017-09-18 10:38:42 +0800

    update

--- src/models/booknews.go
+++ src/models/booknews.go
@@ -20,6 +20,20 @@ type Booknews struct {
 	Update_time int64	`json:"update_time"`
 }
 
+type BooknewsList struct {
+	Newid       string  `json:"newid" orm:"pk;size(20);column(newid);"`
+	Userid_from	string	`json:"userid_from"`
+	Userid_to	string	`json:"userid_to"`
+	Bookqid	    string	`json:"bookqid"`
+	Books       string  `json:"books"`
+	User_from   string	`json:"user_from"`
+	User_to     string	`json:"user_to"`
+	Order_state uint8   `json:"order_state"`
+	Create_time int64	`json:"create_time"`
+	Update_time int64	`json:"update_time"`
+}
+
+
 func init()  {
 	orm.RegisterModelWithPrefix("lb_",new(Booknews))
 }
